4afc89a794bb8c1dd98b84737d29c813b2ba0513
sssbb1234 <58294502+sssbb1234@users.noreply.github.com>
2019-11-28 21:33:14 +0800

    11/28

--- UnityProject/Assets/Scripts/Bird.cs
+++ UnityProject/Assets/Scripts/Bird.cs
@@ -7,12 +7,25 @@ public class Bird : MonoBehaviour
     [Header("是否死亡")]
     public bool dead;
 
+    public GameObject goScore;
+    public GameObject goGM;
+    //遊戲物件 分數
+    //遊戲物件 管理器
+
+
+
     /// <summary>
     /// 小雞跳躍功能
     /// </summary>
     private void Jump()
     {
-
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            print("按下左鍵");
+            goScore.SetActive(true);
+            goGM.SetActive(true);
+            //顯示物件
+        }
     }
 
     /// <summary>
@@ -30,4 +43,10 @@ public class Bird : MonoBehaviour
     {
 
     }
+    //監聽玩家:滑鼠,鍵盤,搖桿
+    private void Update()
+    {
+        Jump();
+    }
+
 }
---
+++ UnityProject/Assets/Scripts/LearnIf.cs
@@ -0,0 +1,57 @@
+﻿
+using UnityEngine;
+
+public class LearnIf : MonoBehaviour
+{
+    public bool open;
+    //這是一個整數值 連擊
+    public int Combo;
+    private void Start()
+    {
+        //判斷式if(switch)
+        //
+    }
+    void Update()
+    {
+        if (Combo>=100)
+        {
+            print("攻擊4倍");
+        }
+        else if (Combo>=75)
+        {
+            print("攻擊3倍");
+        }
+
+        else if (Combo >= 50)
+        {
+            print("攻擊2倍");
+        }
+        else
+        {
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
